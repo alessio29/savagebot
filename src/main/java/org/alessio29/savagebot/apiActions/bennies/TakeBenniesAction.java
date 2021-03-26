55cc6694eef4b8b855050e263c7fe3e65c181aa1
Alexey Moiseev <alessio29@yandex.ru>
2021-03-26 14:43:30 +0300

    Code cleanup

--- src/main/java/org/alessio29/savagebot/apiActions/bennies/TakeBenniesAction.java
+++ src/main/java/org/alessio29/savagebot/apiActions/bennies/TakeBenniesAction.java
@@ -1,12 +1,12 @@
 package org.alessio29.savagebot.apiActions.bennies;
 
-import org.alessio29.savagebot.bennies.BennyColor;
-import org.alessio29.savagebot.bennies.BennyType;
 import org.alessio29.savagebot.characters.Character;
 import org.alessio29.savagebot.characters.Characters;
 import org.alessio29.savagebot.internal.IMessageReceived;
 import org.alessio29.savagebot.internal.commands.CommandExecutionResult;
-import org.alessio29.savagebot.internal.iterators.*;
+import org.alessio29.savagebot.internal.iterators.ParamsIterator;
+import org.alessio29.savagebot.internal.iterators.TakeBenniesParamsIterator;
+import org.alessio29.savagebot.internal.iterators.TakeColoredBenniesParamsIterator;
 import org.alessio29.savagebot.internal.utils.ChannelConfig;
 import org.alessio29.savagebot.internal.utils.ChannelConfigs;
 import org.apache.commons.lang.StringUtils;
@@ -57,41 +57,5 @@ public class TakeBenniesAction {
         }
         return new CommandExecutionResult("Taken bennies from character(s): "+ StringUtils.join(taken, ", "), args.length + 1);
 
-
-
-/* ==============================================
-        BennyType bType = ChannelConfigs.getChannelConfig(message.getChannelId()).getBennyType();
-        if (bType.equals(BennyType.DEADLANDS)) {
-            if (args.length < 2) {
-                return new CommandExecutionResult("Command syntax: tb <CharName> <bennyColor>", args.length + 1);
-            }
-            String charName = args[0];
-            Character character = Characters.getCharacterByName(message.getGuildId(), message.getChannelId(), charName);
-            String bennyColor = args[1];
-            if (character == null) {
-                return new CommandExecutionResult(charName + " not found.", args.length + 1);
-            }
-            if (!character.hasBennies(bennyColor)) {
-                return new CommandExecutionResult("Character " + charName + " has no such bennies.", args.length + 1);
-            }
-            character.useBenny(bennyColor);
-            BennyColor bc = BennyColor.get(bennyColor);
-            return new CommandExecutionResult("Character " + charName + " used " + bc.toString().toLowerCase() + " benny.", args.length + 1);
-        } else {
-            if (args.length < 1) {
-                return new CommandExecutionResult("Command syntax: tb <CharName>", args.length + 1);
-            }
-            String charName = args[0];
-            Character character = Characters.getCharacterByName(message.getGuildId(), message.getChannelId(), charName);
-            if (character == null) {
-                return new CommandExecutionResult(charName + " not found.", args.length + 1);
-            }
-            if (character.getBennies() <= 0) {
-                return new CommandExecutionResult("Character " + charName + " has no bennies.", args.length + 1);
-            }
-            character.useBenny();
-            return new CommandExecutionResult("Character " + charName + " used benny.", args.length + 1);
-        }
-        */
     }
 }
