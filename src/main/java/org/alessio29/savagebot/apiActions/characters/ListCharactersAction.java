e754fe3ee5567e8244154e3db05b05e54b4cd374
Alexey Moiseev <alessio29@yandex.ru>
2021-03-29 11:48:18 +0300

    Bugfix, added sorting by name

--- src/main/java/org/alessio29/savagebot/apiActions/characters/ListCharactersAction.java
+++ src/main/java/org/alessio29/savagebot/apiActions/characters/ListCharactersAction.java
@@ -10,6 +10,7 @@ import org.alessio29.savagebot.internal.utils.ChannelConfigs;
 import org.alessio29.savagebot.internal.utils.Utils;
 
 import java.util.Collection;
+import java.util.Comparator;
 
 public class ListCharactersAction {
 
@@ -33,13 +34,17 @@ public class ListCharactersAction {
                 rightPad("BENNIES", BENNIES_SIZE).
                 rightPad("STATES", STATES_SIZE).
                 newLine();
-        for (Character chr : chars) {
-            replyBuilder.rightPad(chr.getName(), NAME_SIZE).
-                    rightPad(String.valueOf(Utils.notNullValue(chr.getTokens())), TOKEN_SIZE).
-                    rightPad(chr.getBennyValue(bType), BENNIES_SIZE).
-                    rightPad(Utils.notNullValue(chr.getStatesString()), STATES_SIZE).
-                    newLine();
-        }
+
+        chars.stream().sorted(new Comparator<Character>() {
+            @Override
+            public int compare(Character o1, Character o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        }).forEach(chr -> replyBuilder.rightPad(chr.getName(), NAME_SIZE).
+                rightPad(String.valueOf(Utils.notNullValue(chr.getTokens())), TOKEN_SIZE).
+                rightPad(chr.getBennyValue(bType), BENNIES_SIZE).
+                rightPad(Utils.notNullValue(chr.getStatesString()), STATES_SIZE).
+                newLine());
         replyBuilder.blockQuote();
         return new CommandExecutionResult(replyBuilder.toString(), 2);
     }
