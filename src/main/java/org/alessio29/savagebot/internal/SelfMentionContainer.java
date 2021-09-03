f025acf0e65af67c64a1f8d3c7a170390d7e4bdf
Alexey Moiseev <alessio29@yandex.ru>
2021-09-03 11:56:09 +0300

    bugfix, added bot picture

--- src/main/java/org/alessio29/savagebot/internal/SelfMentionContainer.java
+++ src/main/java/org/alessio29/savagebot/internal/SelfMentionContainer.java
@@ -5,9 +5,6 @@ public class SelfMentionContainer {
     private static String mention = null;
 
     public static void initialize(String mention) {
-//        if (!mention.startsWith("<@!")) {
-//            mention = mention.replace("<@", "<@!");
-//        }
         SelfMentionContainer.mention = mention;
     }
 
